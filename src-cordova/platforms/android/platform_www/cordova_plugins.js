053e29a27e32ad9cb5d12acf7ddb57b1ec35f949
boolean <y133@163.com>
2020-07-12 13:23:14 +0800

    替换原生资源

--- src-cordova/platforms/android/platform_www/cordova_plugins.js
+++ src-cordova/platforms/android/platform_www/cordova_plugins.js
@@ -32,12 +32,21 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "window.StatusBar"
       ]
+    },
+    {
+      "id": "cordova-plugin-app-version.AppVersionPlugin",
+      "file": "plugins/cordova-plugin-app-version/www/AppVersionPlugin.js",
+      "pluginId": "cordova-plugin-app-version",
+      "clobbers": [
+        "cordova.getAppVersion"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-splashscreen": "5.0.4",
     "cordova-plugin-network-information": "2.0.2",
-    "cordova-plugin-statusbar": "2.4.3"
+    "cordova-plugin-statusbar": "2.4.3",
+    "cordova-plugin-app-version": "0.1.9"
   };
 });
