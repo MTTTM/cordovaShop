4ae339b342fcf7322b0922e0554ad01a77032f98
boolean <y133@163.com>
2020-07-14 11:33:13 +0800

    和前一个备注一样

---
+++ src/components/statuBar/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :style="{height:height,width:'100%','z-index':500}" class="statusBar"></div>
+</template>
+<script>
+export default {
+    name:"statuBar",
+    props:{
+        plus:{
+            type:Number,
+            default:0
+        }
+    },
+    computed:{
+        height(){
+            // let p=parseInt(this.plus);
+            // if(isNaN(Number(p))){
+            //     p=0;
+            // }
+            let t=(this.$store.state.cvd.statusHeight)+"px";
+            return t;
+        }
+    }
+}
+</script>
